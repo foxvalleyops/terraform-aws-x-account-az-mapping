5048743d260f4247a3e1385dc831fc8a8b854614
Jacob Johansen <jacob.johansen@virginpulse.com>
2021-03-26 08:36:24 -0500

    module to map az ids across accounts

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "zones" {
+  value = { for zone in keys(var.primary_zone_map) : zone => {
+    "name" = local.account_zone_map[var.primary_zone_map[zone]["ZoneID"]]
+    "id"   = var.primary_zone_map[zone]["ZoneID"]
+  } }
+}
+
